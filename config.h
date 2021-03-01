c8e89d9afcc4f3a35b78999202098294d0cb1eaa
kudrenkoa <alex_kd@protonmail.com>
2021-03-01 22:54:02 +0300

    updates for args

--- config.h
+++ config.h
@@ -65,15 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { cpu_perc,    "|| [CPU %s%] -", NULL },
-
-	{ ram_perc,    " [RAM %s%] -", NULL },
-
+	{ battery_perc, "[BAT %s%] -", "BAT1" },
 	{ run_command, " [VOL%4s] -", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-    
-    //{ keymap,     " [KMAP %s] -", NULL },
-
 	{ datetime,   " [%s] -", "%F %T" },
-
-    {run_command, " [%s] ||", "echo ' '" },
+    	{run_command, " [%s]", "echo ' '" },
 };
